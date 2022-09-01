f167f6bfb1ddc01baf15cf3e5cc7631a75fb40d4
Meelock <elisha.pommert@gmail.com>
2022-09-01 04:05:55 -0700

    Initial code and corrections

---
+++ src/main/java/io/github/meelock/eldritchcrops/EldritchCropsMod.kt
@@ -0,0 +1,10 @@
+package io.github.meelock.eldritchcrops
+
+import org.quiltmc.loader.api.ModContainer
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
+
+class EldritchCropsMod: ModInitializer {
+    override fun onInitialize(mod: ModContainer?) {
+//        TODO("Not yet implemented")
+    }
+}
